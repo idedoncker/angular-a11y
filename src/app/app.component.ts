b68a35373a353cd83edce2f77792b92698fe005d
Ines De Doncker <inesdedoncker@gmail.com>
2025-01-23 14:38:29 +0100

    add skip links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Event, NavigationEnd, Router } from '@angular/router';
+import { distinctUntilChanged, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  skipLinksPath = '';
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(
+        filter((routerEvent: Event) => routerEvent instanceof NavigationEnd),
+        map(({ url }: NavigationEnd) => this.trimmedUrl(url)),
+        distinctUntilChanged()
+      )
+      .subscribe((currentUrl: string) => {
+        this.skipLinksPath = `${currentUrl}#content`;
+      });
+  }
+
+  private trimmedUrl(url: string): string {
+    return url.replace('#content', '');
+  }
 }
